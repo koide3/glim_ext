803abe5a993ae390ea5caeb8150ac17db5e93ac0
k.koide <k.koide@aist.go.jp>
2021-11-01 23:47:57 +0900

    hilti

--- orb_slam_frontend/include/glim_ext/orb_slam_frontend.hpp
+++ orb_slam_frontend/include/glim_ext/orb_slam_frontend.hpp
@@ -1,13 +1,18 @@
 #pragma once
 
 #include <memory>
+#include <Eigen/Core>
+#include <opencv2/opencv.hpp>
 
 namespace glim {
 
 class OrbSLAMFrontend {
 public:
-  OrbSLAMFrontend();
+  OrbSLAMFrontend(bool use_own_imu_topic = false, bool use_own_image_topic = false);
   ~OrbSLAMFrontend();
+
+  void insert_imu(const double stamp, const Eigen::Vector3d& linear_acc, const Eigen::Vector3d& angular_vel);
+  void insert_image(const double stamp, const cv::Mat& image);
 
 private:
   class Impl;
--- orb_slam_frontend/src/glim_ext/orb_slam_frontend.cpp
+++ orb_slam_frontend/src/glim_ext/orb_slam_frontend.cpp
@@ -1,26 +1,189 @@
 #include <glim_ext/orb_slam_frontend.hpp>
 
+#include <atomic>
+#include <thread>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 #include <System.h>
 #include <ImuTypes.h>
+
+#include <glim/util/concurrent_vector.hpp>
+#include <glim/frontend/callbacks.hpp>
+#include <glim_ext/util/config_ext.hpp>
 
 namespace glim {
 
 class OrbSLAMFrontend::Impl {
 public:
-  Impl() {
-    const std::string voc_path = "";
-    const std::string settings_path = "";
+  Impl(bool use_own_imu_topic, bool use_own_image_topic) {
+    glim::Config config(glim::GlobalConfigExt::get_config_path("config_orb_slam"));
+    const std::string data_path = glim::GlobalConfigExt::get_data_path();
+    const std::string voc_path = data_path + "/" + config.param<std::string>("orb_slam", "voc_path", "orb_slam/ORBvoc.txt");
+    const std::string settings_path = "/tmp/orb_slam_settings.yaml";
+
+    write_orb_slam_settings(settings_path);
+
+    using std::placeholders::_1;
+    using std::placeholders::_2;
+    using std::placeholders::_3;
+    if (!use_own_imu_topic) {
+      std::cout << "*** use default imu ***" << std::endl;
+      OdometryEstimationCallbacks::on_insert_imu.add(std::bind(&Impl::on_insert_imu, this, _1, _2, _3));
+    }
+    if (!use_own_image_topic) {
+      std::cout << "*** use default image ***" << std::endl;
+      OdometryEstimationCallbacks::on_insert_image.add(std::bind(&Impl::on_insert_image, this, _1, _2));
+    }
+
+    kill_switch = false;
+    thread = std::thread([this] { frontend_task(); });
     system.reset(new ORB_SLAM3::System(voc_path, settings_path, ORB_SLAM3::System::IMU_MONOCULAR, true));
+  }
+  ~Impl() {
+    kill_switch = true;
+    if (thread.joinable()) {
+      thread.join();
+    }
+  }
+
+  void write_orb_slam_settings(const std::string& settings_path) {
+    glim::Config config(glim::GlobalConfigExt::get_config_path("config_sensors_ext"));
+    const auto intrinsics = config.param("sensors_ext", "camera_intrinsics", std::vector<double>());
+    const auto dist_coeffs = config.param("sensors_ext", "camera_distortion_coeffs", std::vector<double>());
+    const auto image_size = config.param("sensors_ext", "camera_image_size", std::vector<int>());
+
+    const Eigen::Isometry3d T_camera_imu = config.param("sensors_ext", "T_camera_imu", Eigen::Isometry3d::Identity());
+    const Eigen::Matrix<double, 4, 4, Eigen::RowMajor> Tbc = T_camera_imu.inverse().matrix();
+
+    std::ofstream ofs(settings_path);
+
+    ofs << "\%YAML:1.0" << std::endl;
+    ofs << "Camera.type: \"PinHole\"" << std::endl;
+    ofs << "Camera.fx: " << intrinsics[0] << std::endl;
+    ofs << "Camera.fy: " << intrinsics[1] << std::endl;
+    ofs << "Camera.cx: " << intrinsics[2] << std::endl;
+    ofs << "Camera.cy: " << intrinsics[3] << std::endl;
+    ofs << "Camera.k1: " << dist_coeffs[0] << std::endl;
+    ofs << "Camera.k2: " << dist_coeffs[1] << std::endl;
+    ofs << "Camera.p1: " << dist_coeffs[2] << std::endl;
+    ofs << "Camera.p2: " << dist_coeffs[3] << std::endl;
+    // k3?
+    ofs << "Camera.width: " << image_size[0] << std::endl;
+    ofs << "Camera.height: " << image_size[1] << std::endl;
+    ofs << "Camera.fps: " << 10.0 << std::endl;
+    ofs << "Camera.RGB: 1" << std::endl;
+
+    ofs << "Tbc: !!opencv-matrix" << std::endl;
+    ofs << "  rows: 4" << std::endl;
+    ofs << "  cols: 4" << std::endl;
+    ofs << "  dt: f" << std::endl;
+    ofs << "  data:[";
+    for (int i = 0; i < 16; i++) {
+      if (i) {
+        ofs << ", ";
+      }
+      ofs << Tbc.data()[i];
+    }
+    ofs << "]" << std::endl;
+
+    ofs << "IMU.NoiseGyro: 1.7e-1" << std::endl;
+    ofs << "IMU.NoiseAcc: 2.0000e-1" << std::endl;
+    ofs << "IMU.GyroWalk: 1.9393e-02 " << std::endl;
+    ofs << "IMU.AccWalk: 3.0000e-01" << std::endl;
+    ofs << "IMU.Frequency: 200" << std::endl;
+
+    ofs << "ORBextractor.nFeatures: 2000" << std::endl;
+    ofs << "ORBextractor.scaleFactor: 1.2" << std::endl;
+    ofs << "ORBextractor.nLevels: 8" << std::endl;
+    ofs << "ORBextractor.iniThFAST: 20" << std::endl;
+    ofs << "ORBextractor.minThFAST: 7" << std::endl;
+
+    ofs << "Viewer.KeyFrameSize: 0.05" << std::endl;
+    ofs << "Viewer.KeyFrameLineWidth: 1" << std::endl;
+    ofs << "Viewer.GraphLineWidth: 0.9" << std::endl;
+    ofs << "Viewer.PointSize: 2" << std::endl;
+    ofs << "Viewer.CameraSize: 0.08" << std::endl;
+    ofs << "Viewer.CameraLineWidth: 3" << std::endl;
+    ofs << "Viewer.ViewpointX: 0" << std::endl;
+    ofs << "Viewer.ViewpointY: -0.7" << std::endl;
+    ofs << "Viewer.ViewpointZ: -1.8" << std::endl;
+    ofs << "Viewer.ViewpointF: 500" << std::endl;
+  }
+
+  void on_insert_imu(const double stamp, const Eigen::Vector3d& linear_acc, const Eigen::Vector3d& angular_vel) {
+    Eigen::Matrix<double, 7, 1> imu_frame;
+    imu_frame << stamp, linear_acc, angular_vel;
+    input_imu_queue.push_back(imu_frame);
+  }
+
+  void on_insert_image(const double stamp, const cv::Mat& image) { input_image_queue.push_back(std::make_pair(stamp, image)); }
+
+  void frontend_task() {
+    std::deque<std::pair<double, cv::Mat>> image_queue;
+    std::deque<Eigen::Matrix<double, 7, 1>, Eigen::aligned_allocator<Eigen::Matrix<double, 7, 1>>> imu_queue;
+
+    while (!kill_switch) {
+      auto new_images = input_image_queue.get_all_and_clear();
+      auto new_imu_frames = input_imu_queue.get_all_and_clear();
+
+      image_queue.insert(image_queue.end(), new_images.begin(), new_images.end());
+      imu_queue.insert(imu_queue.end(), new_imu_frames.begin(), new_imu_frames.end());
+
+      std::cout << "image:" << image_queue.size() << " imu:" << imu_queue.size() << std::endl;
+
+      if (image_queue.empty() || imu_queue.empty()) {
+        continue;
+      }
+
+      if (image_queue.front().first > imu_queue.back()[0]) {
+        continue;
+      }
+
+      const double image_stamp = image_queue.front().first;
+      const auto image = image_queue.front().second;
+      image_queue.pop_front();
+
+      std::vector<ORB_SLAM3::IMU::Point> imu_measurements;
+
+      int imu_cursor = 0;
+      while (imu_cursor < imu_queue.size() && imu_queue[imu_cursor][0] < image_stamp) {
+        const double stamp = imu_queue[imu_cursor][0];
+        const Eigen::Vector3d acc = imu_queue[imu_cursor].block<3, 1>(1, 0);
+        const Eigen::Vector3d gyro = imu_queue[imu_cursor].block<3, 1>(4, 0);
+
+        imu_measurements.push_back(ORB_SLAM3::IMU::Point(cv::Point3f(acc[0], acc[1], acc[2]), cv::Point3f(gyro[0], gyro[1], gyro[2]), stamp));
+        imu_cursor++;
+      }
+
+      imu_queue.erase(imu_queue.begin(), imu_queue.begin() + imu_cursor);
+      cv::Mat T_world_camera = system->TrackMonocular(image, image_stamp, imu_measurements);
+      std::cout << "imu:" << imu_measurements.size() << std::endl;
+    }
   }
 
 private:
   std::unique_ptr<ORB_SLAM3::System> system;
+
+  ConcurrentVector<std::pair<double, cv::Mat>> input_image_queue;
+  ConcurrentVector<Eigen::Matrix<double, 7, 1>, Eigen::aligned_allocator<Eigen::Matrix<double, 7, 1>>> input_imu_queue;
+
+  std::atomic_bool kill_switch;
+  std::thread thread;
 };
 
-OrbSLAMFrontend::OrbSLAMFrontend() {
-  impl.reset(new Impl);
+OrbSLAMFrontend::OrbSLAMFrontend(bool use_own_imu_topic, bool use_own_image_topic) {
+  impl.reset(new Impl(use_own_imu_topic, use_own_image_topic));
 }
 
 OrbSLAMFrontend::~OrbSLAMFrontend() {}
+
+void OrbSLAMFrontend::insert_imu(const double stamp, const Eigen::Vector3d& linear_acc, const Eigen::Vector3d& angular_vel) {
+  impl->on_insert_imu(stamp, linear_acc, angular_vel);
+}
+
+void OrbSLAMFrontend::insert_image(const double stamp, const cv::Mat& image) {
+  impl->on_insert_image(stamp, image);
+}
 
 }  // namespace glim
