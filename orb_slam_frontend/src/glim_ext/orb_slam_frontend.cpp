7f87fe5e8be24106e8b3e468aff4b6b0a65aca17
k.koide <k.koide@aist.go.jp>
2021-11-07 19:10:14 +0900

    orb_slam_frontend working on hilti

--- orb_slam_frontend/src/glim_ext/orb_slam_frontend.cpp
+++ orb_slam_frontend/src/glim_ext/orb_slam_frontend.cpp
@@ -8,27 +8,55 @@
 #include <System.h>
 #include <ImuTypes.h>
 
+#include <gtsam/inference/Symbol.h>
+#include <gtsam/geometry/Pose3.h>
 #include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
 
 #include <glim/common/callbacks.hpp>
-#include <glim/util/concurrent_vector.hpp>
 #include <glim/frontend/callbacks.hpp>
 #include <glim_ext/util/config_ext.hpp>
+#include <glim/util/concurrent_vector.hpp>
 
 namespace glim {
+
+using gtsam::symbol_shorthand::X;
+
+struct VIOFrame {
+public:
+  using Ptr = std::shared_ptr<VIOFrame>;
+  using ConstPtr = std::shared_ptr<const VIOFrame>;
+
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  VIOFrame(const double stamp) : stamp(stamp), valid(false), T_vodom_base(Eigen::Isometry3d::Identity()) {}
+  VIOFrame(const double stamp, const Eigen::Isometry3d& T_vodom_base) : stamp(stamp), valid(true), T_vodom_base(T_vodom_base) {}
+
+public:
+  double stamp;
+  bool valid;
+  Eigen::Isometry3d T_vodom_base;
+};
 
 class OrbSLAMFrontend::Impl {
 public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
   Impl(bool use_own_imu_topic, bool use_own_image_topic) {
-    glim::Config config(glim::GlobalConfigExt::get_config_path("config_orb_slam"));
-    enable_imu = config.param<bool>("orb_slam", "enable_imu", true);
+    glim::Config sensors_config(glim::GlobalConfig::get_config_path("config_sensors"));
+    glim::Config sensors_ext_config(glim::GlobalConfigExt::get_config_path("config_sensors_ext"));
+    glim::Config orb_slam_config(glim::GlobalConfigExt::get_config_path("config_orb_slam"));
+
+    const auto T_lidar_base = sensors_config.param<Eigen::Isometry3d>("sensors", "T_lidar_imu", Eigen::Isometry3d::Identity());
+    const auto T_lidar_camera = sensors_ext_config.param<Eigen::Isometry3d>("sensors_ext", "T_lidar_camera", Eigen::Isometry3d::Identity());
+    T_base_camera = T_lidar_base.inverse() * T_lidar_camera;
+
+    enable_imu = orb_slam_config.param<bool>("orb_slam", "enable_imu", true);
     const std::string data_path = glim::GlobalConfigExt::get_data_path();
-    const std::string voc_path = data_path + "/" + config.param<std::string>("orb_slam", "voc_path", "orb_slam/ORBvoc.txt");
+    const std::string voc_path = data_path + "/" + orb_slam_config.param<std::string>("orb_slam", "voc_path", "orb_slam/ORBvoc.txt");
     const std::string settings_path = "/tmp/orb_slam_settings.yaml";
 
-    write_orb_slam_settings(settings_path, config);
+    write_orb_slam_settings(settings_path, sensors_config, sensors_ext_config, orb_slam_config);
 
     notify(INFO, "[orb_slam] Starting ORB_SLAM...");
     ORB_SLAM3::System::eSensor sensor = enable_imu ? ORB_SLAM3::System::IMU_MONOCULAR : ORB_SLAM3::System::MONOCULAR;
@@ -38,7 +66,8 @@ public:
     using std::placeholders::_1;
     using std::placeholders::_2;
     using std::placeholders::_3;
-    OdometryEstimationCallbacks::on_insert_frame.add(std::bind(&Impl::on_insert_frame, this, _1));
+    OdometryEstimationCallbacks::on_new_frame.add(std::bind(&Impl::on_new_frame, this, _1));
+    OdometryEstimationCallbacks::on_smoother_update.add(std::bind(&Impl::on_smoother_update, this, _1, _2, _3));
 
     if (!use_own_imu_topic) {
       std::cout << "*** use default imu ***" << std::endl;
@@ -61,14 +90,13 @@ public:
     }
   }
 
-  void write_orb_slam_settings(const std::string& settings_path, const glim::Config& orb_slam_config) {
-    glim::Config sensors_config(glim::GlobalConfigExt::get_config_path("config_sensors_ext"));
-    const auto intrinsics = sensors_config.param("sensors_ext", "camera_intrinsics", std::vector<double>());
-    const auto dist_model = sensors_config.param<std::string>("sensors_ext", "camera_distortion_model", "pinhole");
-    const auto dist_coeffs = sensors_config.param("sensors_ext", "camera_distortion_coeffs", std::vector<double>());
-    const auto image_size = sensors_config.param("sensors_ext", "camera_image_size", std::vector<int>());
+  void write_orb_slam_settings(const std::string& settings_path, const glim::Config& sensors_config, const glim::Config& sensors_ext_config, const glim::Config& orb_slam_config) {
+    const auto intrinsics = sensors_ext_config.param("sensors_ext", "camera_intrinsics", std::vector<double>());
+    const auto dist_model = sensors_ext_config.param<std::string>("sensors_ext", "camera_distortion_model", "pinhole");
+    const auto dist_coeffs = sensors_ext_config.param("sensors_ext", "camera_distortion_coeffs", std::vector<double>());
+    const auto image_size = sensors_ext_config.param("sensors_ext", "camera_image_size", std::vector<int>());
 
-    const Eigen::Isometry3d T_imu_camera = sensors_config.param("sensors_ext", "T_imu_camera", Eigen::Isometry3d::Identity());
+    const Eigen::Isometry3d T_imu_camera = sensors_ext_config.param("sensors_ext", "T_imu_camera", Eigen::Isometry3d::Identity());
     const Eigen::Matrix<double, 4, 4, Eigen::RowMajor> Tbc = T_imu_camera.matrix();
 
     std::ofstream ofs(settings_path);
@@ -111,10 +139,10 @@ public:
     }
     ofs << "]" << std::endl;
 
-    ofs << "IMU.NoiseGyro: " << sensors_config.param<double>("sensors_ext", "imu_gyro_noise", 0.01) << std::endl;
-    ofs << "IMU.NoiseAcc: " << sensors_config.param<double>("sensors_ext", "imu_acc_noise", 0.01) << std::endl;
-    ofs << "IMU.GyroWalk: " << sensors_config.param<double>("sensors_ext", "imu_int_noise", 0.01) << std::endl;
-    ofs << "IMU.AccWalk: " << sensors_config.param<double>("sensors_ext", "imu_int_noise", 0.01) << std::endl;
+    ofs << "IMU.NoiseGyro: " << sensors_ext_config.param<double>("sensors_ext", "imu_gyro_noise", 0.01) << std::endl;
+    ofs << "IMU.NoiseAcc: " << sensors_ext_config.param<double>("sensors_ext", "imu_acc_noise", 0.01) << std::endl;
+    ofs << "IMU.GyroWalk: " << sensors_ext_config.param<double>("sensors_ext", "imu_int_noise", 0.01) << std::endl;
+    ofs << "IMU.AccWalk: " << sensors_ext_config.param<double>("sensors_ext", "imu_int_noise", 0.01) << std::endl;
     ofs << "IMU.Frequency: " << orb_slam_config.param<int>("orb_slam", "imu_freq", 100) << std::endl;
 
     ofs << "ORBextractor.nFeatures: " << orb_slam_config.param<int>("orb_slam", "num_features", 1000) << std::endl;
@@ -143,15 +171,22 @@ public:
 
   void on_insert_image(const double stamp, const cv::Mat& image) { input_image_queue.push_back(std::make_pair(stamp, image)); }
 
-  void on_insert_frame(const PreprocessedFrame::Ptr& frame) {
+  void on_new_frame(const EstimationFrame::ConstPtr& frame) {
+    input_odom_frames.push_back(frame);
+
     if (num_queued_images >= 2) {
       std::this_thread::sleep_for(std::chrono::milliseconds(num_queued_images * 2));
     }
 
-    while (num_queued_images > 60) {
+    while (num_queued_images > 10) {
       notify(INFO, "[orb_slam] Stopping the frontend");
       std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+  }
+
+  void on_smoother_update(gtsam_ext::IncrementalFixedLagSmootherExt& isam2, gtsam::NonlinearFactorGraph& new_factors, gtsam::Values& new_values) {
+    auto factors = new_factors_queue.get_all_and_clear();
+    new_factors.add(factors);
   }
 
   void frontend_task() {
@@ -161,9 +196,13 @@ public:
     while (!kill_switch) {
       auto new_images = input_image_queue.get_all_and_clear();
       auto new_imu_frames = input_imu_queue.get_all_and_clear();
+      auto new_odom_frames = input_odom_frames.get_all_and_clear();
 
       image_queue.insert(image_queue.end(), new_images.begin(), new_images.end());
       imu_queue.insert(imu_queue.end(), new_imu_frames.begin(), new_imu_frames.end());
+      odom_frames_queue.insert(odom_frames_queue.end(), new_odom_frames.begin(), new_odom_frames.end());
+
+      create_vio_factors();
 
       num_queued_images = image_queue.size();
       if (image_queue.empty() || imu_queue.empty() || image_queue.front().first > imu_queue.back()[0]) {
@@ -196,31 +235,123 @@ public:
         imu_cursor++;
       }
 
+      cv::Mat T_camera_vodom;
+
       imu_queue.erase(imu_queue.begin(), imu_queue.begin() + imu_cursor);
       if (enable_imu) {
-        cv::Mat T_world_camera = system->TrackMonocular(gray, image_stamp, imu_measurements);
+        T_camera_vodom = system->TrackMonocular(gray, image_stamp, imu_measurements);
       } else {
-        cv::Mat T_world_camera = system->TrackMonocular(gray, image_stamp);
+        T_camera_vodom = system->TrackMonocular(gray, image_stamp);
       }
 
-      // if (!T_world_camera.data) {
-      //   image_queue.clear();
-      // }
+      if (!T_camera_vodom.data) {
+        vodom_frames_queue.push_back(VIOFrame::Ptr(new VIOFrame(image_stamp)));
+        continue;
+      }
+
+      Eigen::Isometry3f T_vodom_camera(Eigen::Matrix<float, 4, 4, Eigen::RowMajor>(reinterpret_cast<float*>(T_camera_vodom.data)).inverse());
+      Eigen::Isometry3d T_vodom_base = T_vodom_camera.cast<double>() * T_base_camera.inverse();
+      vodom_frames_queue.push_back(VIOFrame::Ptr(new VIOFrame(image_stamp, T_vodom_base)));
     }
   }
 
+  void create_vio_factors() {
+    if (odom_frames_queue.size() < 2 || vodom_frames_queue.size() < 2) {
+      return;
+    }
+
+    if (odom_frames_queue[1]->stamp > vodom_frames_queue.back()->stamp) {
+      return;
+    }
+
+    if (odom_frames_queue[0]->stamp < vodom_frames_queue.front()->stamp) {
+      odom_frames_queue.pop_front();
+      return create_vio_factors();
+    }
+
+    int vodom_left = 0;
+    while (vodom_frames_queue[vodom_left + 1]->stamp < odom_frames_queue[0]->stamp) {
+      vodom_left++;
+    }
+
+    int vodom_right = vodom_left;
+    while (vodom_frames_queue[vodom_right]->stamp < odom_frames_queue[1]->stamp) {
+      vodom_right++;
+    }
+
+    const auto interpolate = [](const VIOFrame::ConstPtr& f0, const VIOFrame::ConstPtr& f1, const double stamp) {
+      if (f0->stamp > stamp || f1->stamp < stamp) {
+        std::cerr << "invalid state!!!!" << std::endl;
+        abort();
+      }
+
+      const double p = (stamp - f0->stamp) / (f1->stamp - f0->stamp);
+      const auto& pose0 = f0->T_vodom_base;
+      const auto& pose1 = f1->T_vodom_base;
+
+      const Eigen::Quaterniond q0(pose0.linear());
+      const Eigen::Quaterniond q1(pose1.linear());
+      const Eigen::Vector3d t0 = pose0.translation();
+      const Eigen::Vector3d t1 = pose1.translation();
+
+      Eigen::Isometry3d interpolated = Eigen::Isometry3d::Identity();
+      interpolated.linear() = q0.slerp(p, q1).toRotationMatrix();
+      interpolated.translation() = (1.0 - p) * t0 + p * t1;
+      return interpolated;
+    };
+
+    if (vodom_frames_queue[vodom_left]->valid && vodom_frames_queue[vodom_left + 1]->valid && vodom_frames_queue[vodom_right - 1]->valid && vodom_frames_queue[vodom_right]) {
+      const Eigen::Isometry3d pose0 = interpolate(vodom_frames_queue[vodom_left], vodom_frames_queue[vodom_left + 1], odom_frames_queue[0]->stamp);
+      const Eigen::Isometry3d pose1 = interpolate(vodom_frames_queue[vodom_right - 1], vodom_frames_queue[vodom_right], odom_frames_queue[1]->stamp);
+      const Eigen::Isometry3d vodom_delta = pose0.inverse() * pose1;
+      const Eigen::Isometry3d odom_delta = odom_frames_queue[0]->T_world_sensor().inverse() * odom_frames_queue[1]->T_world_sensor();
+
+      const Eigen::Isometry3d error = odom_delta.inverse() * vodom_delta;
+      const double error_angle = Eigen::AngleAxisd(error.linear()).angle();
+      const double error_trans = error.translation().norm();
+      const double displacement = odom_delta.translation().norm();
+
+      notify(INFO, (boost::format("[orb_slam] new_factor delta:%.3f[m] err_t:%.3f[m] err_r:%.3f[rad]") % displacement % error_trans % error_angle).str());
+
+      gtsam::Vector6 inf_scale;
+      inf_scale << 1e6, 1e6, 1e6, 1e3, 1e3, 1e3;
+      auto factor = gtsam::make_shared<gtsam::BetweenFactor<gtsam::Pose3>>(
+        X(odom_frames_queue[0]->id),
+        X(odom_frames_queue[1]->id),
+        gtsam::Pose3(vodom_delta.matrix()),
+        gtsam::noiseModel::Isotropic::Precisions(inf_scale));
+      new_factors_queue.push_back(factor);
+    }
+
+    odom_frames_queue.pop_front();
+    vodom_frames_queue.erase(vodom_frames_queue.begin(), vodom_frames_queue.begin() + vodom_right - 1);
+    notify(INFO, (boost::format("[orb_slam] odom_queue:%d vodom_queue:%d") % odom_frames_queue.size() % vodom_frames_queue.size()).str());
+
+    return create_vio_factors();
+  }
+
 private:
+  Eigen::Isometry3d T_base_camera;
+
   bool enable_imu;
   cv::Ptr<cv::CLAHE> clahe;
   std::unique_ptr<ORB_SLAM3::System> system;
 
+  // input queues
   ConcurrentVector<std::pair<double, cv::Mat>> input_image_queue;
   ConcurrentVector<Eigen::Matrix<double, 7, 1>, Eigen::aligned_allocator<Eigen::Matrix<double, 7, 1>>> input_imu_queue;
+  ConcurrentVector<EstimationFrame::ConstPtr> input_odom_frames;
 
-  std::atomic_bool kill_switch;
-  std::thread thread;
+  // output queue
+  ConcurrentVector<gtsam::NonlinearFactor::shared_ptr> new_factors_queue;
+
+  // internal queues
+  std::deque<EstimationFrame::ConstPtr> odom_frames_queue;
+  std::deque<VIOFrame::ConstPtr> vodom_frames_queue;
 
   std::atomic_int num_queued_images;
+  std::atomic_bool kill_switch;
+  std::thread thread;
 };
 
 OrbSLAMFrontend::OrbSLAMFrontend(bool use_own_imu_topic, bool use_own_image_topic) {
